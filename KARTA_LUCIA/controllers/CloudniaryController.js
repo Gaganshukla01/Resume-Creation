c617e099ec77f1f352f88c317e60d40c5e879d51
Gaganshukla <gaganshukla@gmail.com>
2024-12-19 23:42:16 +0530

    initial commit

---
+++ KARTA_LUCIA/controllers/CloudniaryController.js
@@ -0,0 +1,37 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: "dv2w3pig9",
+  api_key: "735269986953769",
+  api_secret: "vBoLf-XGl0S74bRuR_Ab6kLpADA"
+});
+
+const uploadImageCloud = (req, res, next) => {
+  if (!req.files || !req.files.photo || req.files.photo.length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+
+  const files = req.files.photo; 
+  const uploadPromises = [];
+  const imagesToUpload = files.length > 12 ? files.slice(0, 12) : files;
+  imagesToUpload.forEach(file => {
+    const uploadPromise = new Promise((resolve, reject) => {
+      cloudinary.uploader.upload(file.tempFilePath, { format: 'jpg' }, (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(result.url);
+      });
+    });
+    uploadPromises.push(uploadPromise);
+  });
+  Promise.all(uploadPromises)
+    .then(urls => {
+      res.send(urls); 
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+};
+
+module.exports = { uploadImageCloud };
